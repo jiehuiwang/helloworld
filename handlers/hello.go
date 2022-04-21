93b21987e316eb15846971f05cd27d834e9fb09f
shi.yang509 <shi.yang509@chinaredstar.com>
2022-04-21 18:55:49 +0800

    fix

--- handlers/hello.go
+++ handlers/hello.go
@@ -2,14 +2,15 @@ package handlers
 
 import (
 	"net/http"
+	"os"
 )
 
 // Hello returns "Hello World!" to the client
 func Hello(w http.ResponseWriter, r *http.Request) {
-	//	value, ok := os.LookupEnv("MESSAGE")
+	value, ok := os.LookupEnv("MESSAGE")
 	w.WriteHeader(http.StatusOK)
 	if ok {
-		w.Write([]byte("Hello!" + value))
+		w.Write([]byte("Hello!" + value + "xiaohuihui"))
 	} else {
 		w.Write([]byte("Hello! world"))
 	}
